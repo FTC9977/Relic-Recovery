1f4e93a1dcf036ccc0318949355537c041b1edab
minemaster12 <charwood.stars12@gmail.com>
2017-09-09 07:08:40 -0400

    Test from Calebs Andriod Studio, now placing a third comment in drive-train code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrain_test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/driveTrain_test.java
@@ -6,6 +6,8 @@ package org.firstinspires.ftc.teamcode;
 
 /*  Caleb Added Notes from his Andriod Studio **/
 /*  Caleb adding notes under his new branch 'Caleb-branch' */
+/*  A third comment under Calebs Branch */
+
 
 
 public class driveTrain_test {
