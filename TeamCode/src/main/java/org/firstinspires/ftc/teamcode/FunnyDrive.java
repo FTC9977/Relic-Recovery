3e261f0e265f0a1df593a20fa2b5517ebfd8cc25
FTC9977 <eharwood6311@gmail.com>
2017-09-12 13:46:40 -0400

    This file is a new Drive Train file to cause the robot to do funny things.
    Added new code to detect 2 out 3 Pictogrpahs...
    
    added new servo code function to control arm...

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/FunnyDrive.java
@@ -0,0 +1,11 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by eharwood on 9/12/17.
+ */
+
+public class FunnyDrive {
+
+    /*  This is a test of adding a new Java File to Erics-branch only */
+
+}
