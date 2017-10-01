6057555eae603687fc18545630435ff3ee868daa
minemaster12 <charwood.stars12@gmail.com>
2017-10-01 17:16:29 -0400

    hi

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/hi_github.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+/**
+ * Created by caleb on 10/1/17.
+ */
+// teaching the two the names below 
+public class hi_github {
+
+    // hi Caleb and Darya
+    // how's is your day
+
+}
