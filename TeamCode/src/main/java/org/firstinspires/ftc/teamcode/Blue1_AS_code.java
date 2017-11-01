9e031f934bda9a2b7e31f523777b23b22374d0b8
minemaster12 <charwood.stars12@gmail.com>
2017-11-01 12:59:12 -0400

    code for the blue side of the corner

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Blue1_AS_code.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+/**
+ * Created by caleb on 10/31/17.
+ */
+
+/*
+  forward = N, back = S, Right = E, Left = W, AND O = Robot
+                     360'/0'
+                        N
+                     NW | NE
+                       \|/
+              270' W -- O -- E 90'
+                       /|\
+                     SW | SE
+                        S
+                       180'
+   This is a compass for the robot. So it is easier to understand on which directions for the robot.
+
+
+*/
+
+
+@Autonomous(name = "blue1",group = "MAS")
+
+public class Blue1_AS_code extends LinearOpMode{
+    HardwareMap_Mecanum_AS MAS = new HardwareMap_Mecanum_AS();
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        MAS.init(hardwareMap);
+
+        MAS.Left_Front.setPower(0);
+        MAS.Left_Back.setPower(0);
+        MAS.Right_Back.setPower(0);
+        MAS.Right_Front.setPower(0);
+
+        waitForStart();
+
+        MAS.N(1);
+        sleep(450);
+        MAS.N(0);
+
+        sleep(500);
+
+        MAS.E(.5);
+        sleep(500);
+        MAS.E(0);
+    }
+}
